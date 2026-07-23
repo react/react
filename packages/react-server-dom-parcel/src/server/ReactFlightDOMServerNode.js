d2e8462636c6b00badaf8cda19da68ff5f12545e
Sebastian Sebbie Silbermann <sebastian.silbermann@vercel.com>
2026-07-23 22:53:55 +0200

    [FlightReply] Avoid unhandled rejections in `decodeReplyFromAsyncIterable`

--- packages/react-server-dom-parcel/src/server/ReactFlightDOMServerNode.js
+++ packages/react-server-dom-parcel/src/server/ReactFlightDOMServerNode.js
@@ -765,7 +765,7 @@ export function decodeReplyFromAsyncIterable<T>(
       } else {
         resolveFile(response, name, value);
       }
-      iterator.next().then(progress, error);
+      iterator.next().then(progress).catch(error);
     }
   }
   function error(reason: Error) {
@@ -777,7 +777,7 @@ export function decodeReplyFromAsyncIterable<T>(
     }
   }
 
-  iterator.next().then(progress, error);
+  iterator.next().then(progress).catch(error);
 
   return getRoot(response);
 }
--- packages/react-server-dom-turbopack/src/server/ReactFlightDOMServerNode.js
+++ packages/react-server-dom-turbopack/src/server/ReactFlightDOMServerNode.js
@@ -759,7 +759,7 @@ function decodeReplyFromAsyncIterable<T>(
       } else {
         resolveFile(response, name, value);
       }
-      iterator.next().then(progress, error);
+      iterator.next().then(progress).catch(error);
     }
   }
   function error(reason: Error) {
@@ -771,7 +771,7 @@ function decodeReplyFromAsyncIterable<T>(
     }
   }
 
-  iterator.next().then(progress, error);
+  iterator.next().then(progress).catch(error);
 
   return getRoot(response);
 }
