38b968c61f243167cf0f047586abffef075c706b
lazerg <lazerg2@gmail.com>
2026-09-23 06:42:41 +0500

    [Fizz] Fix createFastHash in FIPS environments

---
+++ packages/react-server/src/__tests__/ReactServerStreamConfigNode-test.js
@@ -0,0 +1,40 @@
+/**
+ * Copyright (c) Meta Platforms, Inc. and affiliates.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ *
+ * @emails react-core
+ * @jest-environment node
+ */
+
+'use strict';
+
+describe('ReactServerStreamConfigNode', () => {
+  beforeEach(() => {
+    jest.resetModules();
+  });
+
+  it('hashes with md5', () => {
+    const crypto = require('crypto');
+    const {createFastHash} = require('../ReactServerStreamConfigNode');
+
+    expect(createFastHash('hello')).toBe(
+      crypto.createHash('md5').update('hello').digest('hex'),
+    );
+  });
+
+  it('hashes without md5 when FIPS mode is enabled', () => {
+    jest.mock('crypto', () => ({
+      ...jest.requireActual('crypto'),
+      getFips: () => 1,
+      createHash: () => {
+        throw new Error('md5 is not available in FIPS mode');
+      },
+    }));
+    const {createFastHashJS} = require('../createFastHashJS');
+    const {createFastHash} = require('../ReactServerStreamConfigNode');
+
+    expect(createFastHash('hello')).toBe(createFastHashJS('hello'));
+  });
+});
